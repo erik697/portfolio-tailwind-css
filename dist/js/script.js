7cd73101eb8603bf34b889e3acd73d27af6e3352
ERIK SUARDIANA <eriksuardiana644@gmail.com>
2024-01-15 21:34:30 +0800

    update dark mode plus perbaikan fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,22 +1,60 @@
 // navbar fixed
-window.onscroll = function(){
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#toTop");
 
-    if(window.pageYOffset > fixedNav){
-        console.log(window.pageYOffset );
-        console.log('ini scroll', fixedNav)
-        header.classList.add('navbar-fixed');
-    }
-    else{
-        header.classList.remove('navbar-fixed')
-    }
-}
+  if (window.pageYOffset > fixedNav) {
+    console.log(window.pageYOffset);
+    console.log("ini scroll", fixedNav);
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
+  }
+};
 // hamburger
-const humberger = document.querySelector('#humberger');
-const navMenu = document.querySelector('#nav-menu');
+const humberger = document.querySelector("#humberger");
+const navMenu = document.querySelector("#nav-menu");
 
-humberger.addEventListener('click', function(){
-    humberger.classList.toggle('humberger-active')
-    navMenu.classList.toggle('hidden')
-})
+humberger.addEventListener("click", function () {
+  humberger.classList.toggle("humberger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// klik diluar humberger
+window.addEventListener("click", function (e) {
+  if (e.target != navMenu && e.target != humberger) {
+    humberger.classList.remove("humberger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// darkmode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// pindahkan posisi toggle terhadap mode
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
